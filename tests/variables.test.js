f6bcc23985865384189367772380bd92e9b7728d
ubermanu <manu@paca.happy-dev.fr>
2022-11-19 03:00:05 +0100

    feat: add test for variable assigned to prop

--- tests/variables.test.js
+++ tests/variables.test.js
@@ -6,3 +6,8 @@ test('Variables should be correctly transformed', t => {
     t.is(transformSync('@primary__color: blue;'), '$primary__color: blue;');
     t.is(transformSync('@primary__color: @color;'), '$primary__color: $color;');
 });
+
+test('Variables should be correctly transformed in selectors', t => {
+    t.is(transformSync('.test { color: @color; }'), '.test { color: $color; }');
+    t.is(transformSync('.test { color: @primary__color; }'), '.test { color: $primary__color; }');
+});
